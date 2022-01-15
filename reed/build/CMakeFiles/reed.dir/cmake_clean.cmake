9ff05d80691b7e0250ff373f18e3132cb7fcbaec
Vasily <resecmo@gmail.com>
2022-01-15 17:22:47 +0300

    started reed

---
+++ reed/build/CMakeFiles/reed.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reed.dir/src/main.cpp.o"
+  "reed"
+  "reed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ reed/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << 0 << std::endl;
+
+    return 0;
+}
